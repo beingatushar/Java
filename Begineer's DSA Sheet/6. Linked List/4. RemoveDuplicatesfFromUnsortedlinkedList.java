d663812307eecfd99a0b4759ffd21bed82d43300
Tushar Aggarwal <beingatushar@gmail.com>
2022-01-05 00:37:17 +0530

    Create 4. RemoveDuplicatesfFromUnsortedlinkedList.java

---
+++ Begineer's DSA Sheet/6. Linked List/4. RemoveDuplicatesfFromUnsortedlinkedList.java
@@ -0,0 +1,35 @@
+// https://www.codingninjas.com/codestudio/problems/remove-duplicates-from-unsorted-linked-list_1069331?leftPanelTab=1
+/************************************************************
+ * 
+ * Following is the linked list node structure
+ * 
+ * class LinkedListNode<T> {
+ * T data;
+ * LinkedListNode<T> next;
+ * 
+ * public LinkedListNode(T data) {
+ * this.data = data;
+ * }
+ * }
+ * 
+ ************************************************************/
+public class Solution {
+
+    public static LinkedListNode<Integer> removeDuplicates(LinkedListNode<Integer> head) {
+        LinkedListNode<Integer> dummy = head;
+        while (head != null) {
+            LinkedListNode<Integer> prev = head;
+            int x = head.data;
+            while (prev != null && prev.next != null) {
+                if (prev.next.data == x) {
+                    prev.next = prev.next.next;
+                } else
+                    prev = prev.next;
+            }
+            head = head.next;
+        }
+        return dummy;
+
+    }
+
+}
