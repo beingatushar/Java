461183c68cc672f81c9167857a3998038808b149
Tushar Aggarwal <beingatushar@gmail.com>
2022-01-06 01:52:33 +0530

    Create 08_CountElementsSmallerThanCurrentNumber.java

---
+++ DSA-Bootcamp/5-Arrays/1. Easy-28/08_CountElementsSmallerThanCurrentNumber.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] bucket = new int[102];
+        for(int i=0; i<nums.length; bucket[nums[i]+1]++, i++);
+        for(int i=0; i<101; bucket[i+1] += bucket[i], i++);
+        for(int i=0; i<nums.length; nums[i] = bucket[nums[i]], i++);
+        return nums;
+    }
+}
