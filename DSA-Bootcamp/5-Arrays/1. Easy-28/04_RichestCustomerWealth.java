371e7c2a05ac6f8a7e3aadb8d10e0ad1839f8ba7
Tushar Aggarwal <beingatushar@gmail.com>
2022-01-06 01:52:25 +0530

    Create 04_RichestCustomerWealth.java

---
+++ DSA-Bootcamp/5-Arrays/1. Easy-28/04_RichestCustomerWealth.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/richest-customer-wealth/submissions/
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxsum = 0;
+        for (int i[] : accounts) {
+            int sum = 0;
+            for (int j : i) {
+                sum += j;
+            }
+            if (sum > maxsum) {
+                maxsum = sum;
+            }
+        }
+        return maxsum;
+    }
+}
