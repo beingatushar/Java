a02ce79a6f657d0a4ab9fc9f907d7a6b96991d02
Tushar Aggarwal <beingatushar@gmail.com>
2022-01-12 23:38:12 +0530

    Update 17_TransposeMatrix.java

--- DSA-Bootcamp/5-Arrays/1. Easy-28/17_TransposeMatrix.java
+++ DSA-Bootcamp/5-Arrays/1. Easy-28/17_TransposeMatrix.java
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/transpose-matrix/
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        int n = matrix.length;
-        int[][] ans = new int[n][n];
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] ans = new int[n][m];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 ans[i][j] = matrix[j][i];
             }
         }
