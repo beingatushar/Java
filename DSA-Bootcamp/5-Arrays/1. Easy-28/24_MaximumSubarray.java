53fb16601dadae4cba64c8d6753b08a83334456a
Tushar Aggarwal <beingatushar@gmail.com>
2022-01-14 18:42:49 +0530

    Create 24_MaximumSubarray.java

---
+++ DSA-Bootcamp/5-Arrays/1. Easy-28/24_MaximumSubarray.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/maximum-subarray/submissions/
+// Kadane's Algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
